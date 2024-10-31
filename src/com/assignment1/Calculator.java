5e78012ac2ca39147875b838b55997c7079820d3
AstikPS <astik.sayare@pyramidions.in>
2024-10-31 12:12:51 +0530

    Update calculator

--- src/com/assignment1/Calculator.java
+++ src/com/assignment1/Calculator.java
@@ -1,6 +1,6 @@
 package com.assignment1;
 
-// Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+// Take in two numbers and an operator (+, -, *, /) and calculate the value till the user click on x to terminate the program.
 import java.util.Scanner;
 
 public class Calculator {
@@ -8,27 +8,57 @@ public class Calculator {
     public static void main(String[] args) {
 
         Scanner scn = new Scanner(System.in);
+        while (true){
+            System.out.print("Enter the operator: ");
+            char op = scn.next().trim().charAt(0);
 
-        System.out.print("Select one operator (+, -, *, /): ");
-        char op = scn.next().charAt(0);
+            if (op == '+'){
+                System.out.println("Enter the first number: ");
+                int num1 = scn.nextInt();
+                System.out.println("Enter the second number: ");
+                int num2 = scn.nextInt();
 
-        System.out.print("Enter the first number: ");
-        int num1 = scn.nextInt();
+                System.out.println(num1 + " + " + num2 +" = "+ (num1 + num2));
+            } else if (op == '-'){
+                System.out.println("Enter the first number: ");
+                int num1 = scn.nextInt();
+                System.out.println("Enter the second number: ");
+                int num2 = scn.nextInt();
 
-        System.out.print("Enter the second number: ");
-        int num2 = scn.nextInt();
+                System.out.println(num1 + " - " + num2 +" = "+ (num1 - num2));
+            } else if (op == '*'){
+                System.out.println("Enter the first number: ");
+                int num1 = scn.nextInt();
+                System.out.println("Enter the second number: ");
+                int num2 = scn.nextInt();
 
-        if (op == '+'){
-            System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+                System.out.println(num1 + " * " + num2 +" = "+ (num1 * num2));
+            } else if (op == '%'){
+                System.out.println("Enter the first number: ");
+                int num1 = scn.nextInt();
+                System.out.println("Enter the second number: ");
+                int num2 = scn.nextInt();
+
+                System.out.println(num1 + " % " + num2 +" = "+ (num1 % num2));
+            }else if (op == '/'){
+                System.out.println("Enter the first number: ");
+                int num1 = scn.nextInt();
+                System.out.println("Enter the second number: ");
+                int num2 = scn.nextInt();
+
+                if (num2 != 0){
+                    System.out.println(num1 + " / " + num2 +" = "+ (num1 / num2));
+                }else {
+                    System.out.println("Cannot be divided by 0");
+                }
+
+            }else if (op == 'x' || op == 'X'){
+                System.out.println("Program End");
+                break;
+            }else {
+                System.out.println("Invalid Operator");
+            }
         }
-        else  if (op == '-'){
-            System.out.println(num2 + " - " + num1+ " = " + (num2-num1));
-        }
-        else  if (op == '*'){
-            System.out.println(num1 + " * " + num2+ " = " + (num1*num2));
-        }
-        else  if (op == '/'){
-            System.out.println(num2 + " / " + num1+ " = " + (num2/num1));
-        }
+
     }
 }
