30f129645a7a542bb332916421b276a034f92a26
AstikPS <astik.sayare@pyramidions.in>
2024-11-01 22:33:12 +0530

    Condition and Loop questions

---
+++ src/com/assignment2/conditionsandloops/AreaOfTriangle.java
@@ -0,0 +1,19 @@
+package com.assignment2.conditionsandloops;
+
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter the base : ");
+        double base = scn.nextInt();
+
+        System.out.print("Enter the height : ");
+        double height = scn.nextInt();
+
+        double areaOfTriangle = (base * height )/2;
+
+        System.out.println("Area of Triangle: "+ (int) areaOfTriangle);
+    }
+}
---
+++ src/com/assignment2/conditionsandloops/SumOfAllNumber.java
@@ -0,0 +1,26 @@
+package com.assignment2.conditionsandloops;
+
+import java.util.Scanner;
+
+// Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+public class SumOfAllNumber {
+    static int sumOfAll(){
+        int sum = 0;
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int num = scn.nextInt();
+
+        while (num != 0){
+            sum += num;
+            System.out.print("Enter the number: ");
+            num = scn.nextInt();
+        }
+        return  sum;
+    }
+
+    public static void main(String[] args) {
+        int ans = SumOfAllNumber.sumOfAll();
+        System.out.println("Sum of all the above numbers is: "+ ans);
+    }
+}
